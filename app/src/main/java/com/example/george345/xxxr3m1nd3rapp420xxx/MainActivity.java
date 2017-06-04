0f916ce08525862da58ebe0096e3560e78878979
Maxwell Myers <rodiah2o@yahoo.com>
2017-06-03 20:40:50 -0700

    Initialization

---
+++ app/src/main/java/com/example/george345/xxxr3m1nd3rapp420xxx/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.george345.xxxr3m1nd3rapp420xxx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
